d750a04d54c84e8fef22b3581da609aac911aae5
Pierre Wielders <pierre@wielders.net>
2023-04-25 09:01:53 +0200

    [SYNC]

--- addon/board/sky/build_image.sh
+++ addon/board/sky/build_image.sh
@@ -1,9 +1,9 @@
-rm -rf $BUILD_DIR/ImageBuilder 
-result=$(git clone git@github.com:Metrological/amlogic-image-builder.git $BUILD_DIR/ImageBuilder)
+rm -rf $BUILD_DIR/amlogic-image-builder
+result=$(git clone git@github.com:Metrological/amlogic-image-builder.git $BUILD_DIR/amlogic-image-builder)
 
 if [[ "$result" != "" ]]; then
 	echo "Failed to checkout the Image Deployment creation repository"
 else
 	echo "Ready to create the deployment images from input [$2]...."
-	"$BUILD_DIR/ImageBuilder/create_images.sh" "$2" "$TARGET_DIR" "$BINARIES_DIR"
+	"$BUILD_DIR/amlogic-image-builder/create_images.sh" "$2" "$TARGET_DIR" "$BINARIES_DIR"
 fi
