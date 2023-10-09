e8e362338333b9055708a7daef7afaf7d7f97ab5
Pierre Wielders <pierre@wielders.net>
2023-10-09 12:37:36 +0200

    [SYNC]

--- addon/board/bananapi/bananapi-m2-zero/post-build.sh
+++ addon/board/bananapi/bananapi-m2-zero/post-build.sh
@@ -2,5 +2,6 @@
 BOARD_DIR="$(dirname $0)"
 
 cp $BOARD_DIR/uboot.txt $BINARIES_DIR/uboot.txt
+cp -rp $BOARD_DIR/firmware $TARGET_DIR/lib/firmware
 
 $HOST_DIR/bin/mkimage -C none -A arm -T script -d $BOARD_DIR/boot.cmd $BINARIES_DIR/boot.scr
