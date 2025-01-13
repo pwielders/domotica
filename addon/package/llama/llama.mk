b25f44a49503ca423c1e3ce05a0f3570afce46b3
Pierre Wielders <pierre@wielders.net>
2025-01-13 23:12:15 +0100

    [SYNC]

--- addon/package/llama/llama.mk
+++ addon/package/llama/llama.mk
@@ -6,13 +6,11 @@
 LLAMA_VERSION = 8d59d911711b8f1ba9ec57c4b192ccd2628af033
 LLAMA_SITE = $(call github,ggerganov,llama.cpp,$(LLAMA_VERSION))
 LLAMA_INSTALL_STAGING = YES
-# LLAMA_DEPENDENCIES = 
+LLAMA_DEPENDENCIES = ggml
 
 ifeq ($(BR2_arm)$(BR2_aarch64),y)
 LLAMA_CONF_OPTS += -DCMAKE_C_FLAGS="-mfp16-format=ieee" \
                    -DCMAKE_CXX_FLAGS="-mfp16-format=ieee"
 endif
-
-
 
 $(eval $(cmake-package))
