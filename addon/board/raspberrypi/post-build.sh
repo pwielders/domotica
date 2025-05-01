190f97b30f4b54e66eae82b6587b53a87dc9e691
Pierre Wielders <pierre@wielders.net>
2025-05-01 10:24:41 +0200

    [SYNC] Update after testing

--- addon/board/raspberrypi/post-build.sh
+++ addon/board/raspberrypi/post-build.sh
@@ -19,15 +19,15 @@ fi
 
 if grep -q "^BR2_TARGET_ROOTFS_INITRAMFS=y$" "${BR2_CONFIG}"; then
 
-mkdir -p "${TARGET_DIR}/boot"
-grep -q '^/dev/mmcblk0p1' "${TARGET_DIR}/etc/fstab" || \
-    echo '/dev/mmcblk0p1 /boot vfat defaults 0 0' >> "${TARGET_DIR}/etc/fstab"
-
 mkdir -p "${TARGET_DIR}/root"
 grep -q '^/dev/mmcblk0p2' "${TARGET_DIR}/etc/fstab" || \
     echo '/dev/mmcblk0p2 /root ext4 defaults 0 0' >> "${TARGET_DIR}/etc/fstab"
 
 fi
+
+mkdir -p "${TARGET_DIR}/boot"
+grep -q '^/dev/mmcblk0p1' "${TARGET_DIR}/etc/fstab" || \
+    echo '/dev/mmcblk0p1 /boot vfat defaults 0 0' >> "${TARGET_DIR}/etc/fstab"
 
 if [ ${BLUETOOTH} -gt 0 ]; then
    if ! grep -qE '^enable_uart=1' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
